d771218ede6481a39e554c90ce022260ce94c976
adityraj2311 <119840306+adityraj2311@users.noreply.github.com>
2024-04-24 22:20:19 +0530

    Add files via upload

---
+++ HousingData(DataCleaning).sql
@@ -0,0 +1,199 @@
+/*
+
+                                                         Cleaning Data in SQL Queries
+
+*/
+
+--TABLE 
+Select *
+From dbo.HousingData
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Standardize Date Format
+
+
+Select saleDateConverted, CONVERT(Date,SaleDate)
+From dbo.HousingData
+
+Update HousingData
+SET SaleDate = CONVERT(Date,SaleDate)
+
+-- If it doesn't Update properly
+
+ALTER TABLE HousingData
+Add SaleDateConverted Date;
+
+Update HousingData
+SET SaleDateConverted = CONVERT(Date,SaleDate)
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Populate Property Address data
+
+Select *
+From dbo.HousingData
+--Where PropertyAddress is null
+order by ParcelID
+
+
+
+Select a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL(a.PropertyAddress,b.PropertyAddress)
+From dbo.HousingData a
+JOIN dbo.HousingData b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+
+Update a
+SET PropertyAddress = ISNULL(a.PropertyAddress,b.PropertyAddress)
+From dbo.HousingData a
+JOIN dbo.HousingData b
+	on a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+Where a.PropertyAddress is null
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Breaking out Address into Individual Columns (Address, City, State)
+
+--Property Address
+Select PropertyAddress
+From dbo.HousingData
+--Where PropertyAddress is null
+--order by ParcelID
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) as Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress)) as Address
+
+From dbo.HousingData
+
+
+ALTER TABLE HousingData
+Add PropertySplitAddress Nvarchar(255);
+
+Update HousingData
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+
+ALTER TABLE HousingData
+Add PropertySplitCity Nvarchar(255);
+
+Update HousingData
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 1 , LEN(PropertyAddress))
+
+
+--Owner Address
+
+
+Select OwnerAddress
+From dbo.HousingData
+
+
+Select
+PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+From dbo.HousingData
+
+
+
+ALTER TABLE HousingData
+Add OwnerSplitAddress Nvarchar(255);
+
+Update HousingData
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 3)
+
+
+ALTER TABLE HousingData
+Add OwnerSplitCity Nvarchar(255);
+
+Update HousingData
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
+
+
+
+ALTER TABLE HousingData
+Add OwnerSplitState Nvarchar(255);
+
+Update HousingData
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
+
+
+
+Select *
+From dbo.HousingData
+
+
+
+--------------------------------------------------------------------------------------------------------------------------
+
+-- Change Y and N to Yes and No in "Sold as Vacant" field
+
+
+Select Distinct(SoldAsVacant), Count(SoldAsVacant)
+From dbo.HousingData
+Group by SoldAsVacant
+order by 2
+
+
+
+
+Select SoldAsVacant
+, CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+From .dbo.HousingData
+
+
+
+Update HousingData
+
+ SET SoldAsVacant = CASE When SoldAsVacant = 'Y' THEN 'Yes'
+	   When SoldAsVacant = 'N' THEN 'No'
+	   ELSE SoldAsVacant
+	   END
+
+
+
+
+
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Remove Duplicates
+
+WITH RowNumCTE AS(
+Select *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY
+					UniqueID
+					) row_num
+
+From dbo.HousingData
+--order by ParcelID
+)
+Select *
+From RowNumCTE
+Where row_num > 1
+Order by PropertyAddress
+
+
+
+Select *
+From dbo.HousingData
+
+---------------------------------------------------------------------------------------------------------
+
+
+
+
